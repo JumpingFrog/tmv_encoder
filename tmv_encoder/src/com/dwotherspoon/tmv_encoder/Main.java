a9e0e46c5c789d506304407ea1a746b68514414d
David Wotherspoon <davidwother@hotmail.co.uk>
2013-10-20 10:57:04 +0100

    EGit Test

---
+++ tmv_encoder/src/com/dwotherspoon/tmv_encoder/Main.java
@@ -0,0 +1,13 @@
+package com.dwotherspoon.tmv_encoder;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
